355b59e83a4ee1db00c4c80dfeb2b4056aaf9424
ziyangfan <1229417754@qq.com>
2024-09-13 00:43:01 +0800

    Implement is-same-tree

---
+++ binary-tree/is_same_tree.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val: int=0, left: 'TreeNode | None'=None, right: 'TreeNode | None'=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def is_same_tree(p: TreeNode | None, q: TreeNode | None) -> bool:
+    if not p and not q:
+        return True
+    elif p and q:
+        return p.val == q.val and is_same_tree(p.left, q.left) and is_same_tree(p.right, q.right)
+    return False
