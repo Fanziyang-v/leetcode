0780e3b6ab6ca92c2ef170145acabc1f5d7b27b5
ziyangfan <1229417754@qq.com>
2024-09-13 13:21:02 +0800

    implement is-symmetric-tree

---
+++ binary-tree/is_symmetric.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val: int=0, left: 'TreeNode | None'=None, right: 'TreeNode | None'=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def is_symmetric(root: TreeNode | None) -> bool:
+    if root is None:
+        return True
+    def is_symmetric_helper(p: TreeNode | None, q: TreeNode | None) -> bool:
+        if not p and not q:
+            return True
+        elif p and q:
+            return p.val == q.val and is_symmetric_helper(p.left, q.right) and is_symmetric_helper(p.right, q.left)
+        return False
+    return is_symmetric_helper(root.left, root.right)
