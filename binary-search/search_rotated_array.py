23bc51f78f8d2ff712dd26f24c16f1142c3a77ca
ziyangfan <1229417754@qq.com>
2024-10-15 01:08:37 +0800

    implement search-rotated-array.

---
+++ binary-search/search_rotated_array.py
@@ -0,0 +1,23 @@
+"""
+Search in Rotated Sorted Array.
+
+Link: https://leetcode.cn/problems/search-in-rotated-sorted-array/
+"""
+
+def search(nums: list[int], target: int) -> int:
+    lo, hi = 0, len(nums) - 1
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        if nums[mid] == target:
+            return mid
+        if nums[mid] >= nums[0]:
+            if nums[lo] <= target < nums[mid]:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        else:
+            if nums[mid] < target <= nums[hi]:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+    return -1
