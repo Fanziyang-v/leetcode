a10102d4b4b60c50e5dfbb887f84be69ec4144e7
ziyangfan <1229417754@qq.com>
2024-11-16 02:21:47 +0800

    implement num-decodings.

---
+++ dynamic-programming/num_decodings.py
@@ -0,0 +1,16 @@
+"""
+Decode Ways.
+
+Link: https://leetcode.cn/problems/decode-ways/
+"""
+
+
+def num_decodings(s: str) -> int:
+    n = len(s)
+    dp = [1] + [0] * n
+    for i in range(1, n + 1):
+        if s[i - 1] != "0":
+            dp[i] += dp[i - 1]
+        if i > 1 and s[i - 2] != "0" and s[i - 2 : i] <= "26":
+            dp[i] += dp[i - 2]
+    return dp[n]
